a9b6182d356ca26437351909a108f1819be2e2e3
John McDowell <johnmcdowell0801@gmail.com>
2018-06-12 08:26:28 +0000

    data displaying on scfreen. need to work with user input

--- home/views.py
+++ home/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
+
 import bs4 as bs
 from urllib.request import urlopen
 
-# Set a URL that we'll try to grab some HTML from.
-url = "https://en.wikipedia.org/wiki/Web_scraping"
+def get_index(request): 
 
-# Open the URL and read the HTML content into a string variable
-html_page = urlopen(url)
-html_text = html_page.read()
-
-# Print html_test string
-print(html_text)
+    # sauce = urlopen("https://en.wikipedia.org/wiki/The_Beatles").read()
+    sauce = urlopen("https://en.wikipedia.org/wiki/Nirvana_(band)").read()
+    # sauce = urlopen("https://en.wikipedia.org/wiki/Queens_of_the_Stone_Age").read()
+    # sauce = urlopen("https://en.wikipedia.org/wiki/Kyuss").read()
+    
+    soup = bs.BeautifulSoup(sauce, 'lxml')
+    band_name = soup.find('h1')
+    
+    covers = soup.select('table.infobox a.image img[src]')
+    for cover in covers:
+        cover_link = cover['src'] 
+    
+    table = soup.find('table', class_='infobox')
+    table_rows = table.find_all('tr')
+    for tr in table_rows:
+        table_header = tr.find_all('th')
+        for th in table_header:
+            if th.text == "Genres":
+                genre_row = tr
+    
+    genres = [i.text for i in genre_row.find_all('a')]
+    
+    print(band_name.text)
+    print(cover_link)
+    print(genres)
+    
+    return render(request, "home/index.html", {"band_name": band_name.text, "cover_link": cover_link, "genres": genres})
